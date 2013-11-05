9ca20143f532b8a8fd2d0a9861ec37c8bddad201
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-04 20:57:22 -0600

    index controller created, loads racer

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,20 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/users' do
+  # check that both players entered names
+  if params[:nick1].empty? || params[:nick2].empty?
+    return 'Both players must have nicknames'
+  end
+
+  if Player.create(:nickname => params[:nick1]).invalid?
+    return 'Player 1 name invalid. Enter another name.'
+  end
+
+  if Player.create(:nickname => params[:nick2]).invalid?
+    return 'Player 2 name invalid. Enter another name.'
+  end
+
+  erb :racer
+end
