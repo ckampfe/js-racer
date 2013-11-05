766695bc4a8a85ef43619f83d95471c3f070743c
Clark Kampfe <clark.kampfe@gmail.com>
2013-11-05 11:02:09 -0600

    added columns to create_games

--- db/migrate/20131104153159_create_games.rb
+++ db/migrate/20131104153159_create_games.rb
@@ -3,5 +3,8 @@ class CreateGames < ActiveRecord::Migration
     create_table :games do |t|
       t.timestamps
     end
+
+  add_column :games, :winner_id, :integer
+  add_column :games, :winning_time, :string
   end
 end
