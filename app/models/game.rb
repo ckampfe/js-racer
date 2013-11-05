8157e4e9b7a17a979e1d854e71b296b33b49d531
Clark Kampfe <clark.kampfe@gmail.com>
2013-11-05 17:54:07 -0600

    commented out stupid validation

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
-  validate :max_players, :on => :update
+  # validate :max_players, :on => :update
   has_many :players_games
   has_many :players, :through => :players_games
 end
