ce58399a3a931421431581bc32d151666a7025d9
Clark Kampfe <clark.kampfe@gmail.com>
2013-11-06 00:21:10 -0600

    players race, don't finish though

--- public/js/application.js
+++ public/js/application.js
@@ -1,44 +1,42 @@
 $(document).ready(function() {
 
+  aGame = new Game();
+  player1 = new Player($( "#player1_strip" ).attr( "data-nick" ), 1);
+  player2 = new Player($( "#player2_strip" ).attr( "data-nick" ), 2);
+  players = [player1, player2];
+
+
   /* GAME */
 
-  function Game() = {
+  function Game() {
     this.won = false;
   }
 
   /* game prototype */
 
-  // check if game is over
-  Game.prototype.gameOver? = function() {
-    return this.won; //true || false
-  }
-
-  // end game 
-  Game.prototype.gameOver = function() {
-    this.won = true;
-  }
-
   // listen for keyup events
-  Game.prototype.listen = function() {
+  Game.prototype.keypressListen = function() {
     $( document ).on("keyup", function( event ) {
       switch(event.which) {
         case 80:
-          mover("player1");
+          console.log("80 keypress");
+          console.log(player1.playerNumber);
+          player1.move();
           break;
         case 81:
-          mover("player2");
+          console.log("81 keypress");
+          console.log(player2.playerNumber);
+          player2.move();
           break;
       }
-    }
+    });
   }
 
   // send game data to server
   Game.prototype.sendWinnerInfo = function(winner, time) {
-    var players = { "player1": $( "#player1_strip" ).attr( "data-nick" ),
-                    "player2": $( "#player2_strip" ).attr( "data-nick" )
-    }
-
-    $.post( "/winner", { players,
+    $.post( "/winner", { "players": { "player1": player1.nick,
+                                      "player2": player1.nick 
+                                     },
                          "winner": winner,
                          "time": time/1000,
                          "gameId": $( ".racer_table" ).attr( "data-game-id" ),
@@ -50,30 +48,34 @@ $(document).ready(function() {
     $( "#stats" ).html(reponse);
   }
 
-
-
-
-  /* PLAYER */
-
-  function Player(name) = {
-    this.name = name;
-  }
-
-  /* player prototype */
-
-  // move player
-  Player.prototype.mover = function() {
-    $("#" + this.name + "_strip > td.active".removeClass( "active" ).next( "td" ).addClass( "active" );
-  }
-
   // is player at the last cell?
-  Player.prototype.last = function() {
-    if ($("#" + this.name + "_strip > td").filter( ":last" ).is( ".active" )) {
+  Game.prototype.lastListen = function(player) {
+    if ($("#player" + player.playerNumber + "_strip > td").filter( ":last" ).is( ".active" )) {
+      aGame.gameOver();
       return true;
     } else {
       return false;
     }
   }
 
+  /* PLAYER */
 
+  function Player(nick, playerNumber) {
+    this.nick         = nick;
+    this.playerNumber = playerNumber;
+  }
+
+  /* player prototype */
+
+  // move player
+  Player.prototype.move = function() {
+    $( "#player" + this.playerNumber + "_strip > td.active" ).removeClass( "active" ).next( "td" ).addClass( "active" );
+  }
+
+
+  aGame.keypressListen();
+  for (player of players) {
+    aGame.lastListen(player);
+  }
 });
+
